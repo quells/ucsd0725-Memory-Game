65f7f122d5b6f38d3aa6958e1e7e4366fbf55438
Kai Wells <support@kaiwells.me>
2017-12-09 21:25:11 -0800

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app-root">
+        <header className="primary">
+          <div className="container text-center mt-3">
+            <h1>Memory Game</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          Remember this!
+        </main>
+        <footer>
+          Memory Game
+        </footer>
       </div>
     );
   }
